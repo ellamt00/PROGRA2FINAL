30da99165d0ba423fcd2dbcb58acd5f3650c120f
Juan Fco Blanco <47599100+jblant00@users.noreply.github.com>
2022-01-31 00:39:22 +0100

    Pantalla cambiada

--- Pantalla.java
+++ Pantalla.java
@@ -8,6 +8,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -872,6 +874,24 @@ public class Pantalla extends JFrame implements ActionListener {
 
 
 	class KeyListen implements KeyListener{
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
 
 	}
 
